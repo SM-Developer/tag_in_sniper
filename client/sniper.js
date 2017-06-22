a26efd48a78b44a33aa862307a5f9039fe0e6100
lojpark <lojpark@naver.com>
2017-06-23 01:56:05 +0900

    feat: create screen object

--- client/sniper.js
+++ client/sniper.js
@@ -1,16 +1,23 @@
-var Sniper = function() {
+var Sniper = function(){
     var self = {
         motion: 0,
         state: 'idle'
     }
     
-    self.update = function() {
+    self.update = function(){
         if( state === 'idle' ){
             self.motion = 0;
         }
         else if( state === 'walk' ){
             self.motion++;
         }
+    }
+
+    self.print = function( scr, img, context ){
+        var x = self.x - 16 - scr.x;
+        var y = self.y - 24 - scr.y;
+        var w = 32, h = 48;
+        context.drawImage( img.sniper, 0 * 32, 0 * 48, w, h, x, y, w, h );
     }
 
     return self;
